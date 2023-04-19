c9815e6f488ee14b099362f7c4309f6ddf938e1a
kamijoucen <kamijoucen@foxmail.com>
2023-04-19 19:24:35 +0800

    fix

--- init.el
+++ init.el
@@ -1,5 +1,5 @@
 (add-to-list 'load-path
-	     (expand-file-name (concat user-emacs-directory "lisp")))
+         (expand-file-name (concat user-emacs-directory "lisp")))
 (require 'init-startup)
 (require 'init-elpa)
 (require 'init-package)
