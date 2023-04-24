2b5ada5c7b51fe9a29361023f9fb3465b3dfc423
kamijoucen <kamijoucen@foxmail.com>
2023-04-24 11:12:38 +0800

    font

--- lisp/init-startup.el
+++ lisp/init-startup.el
@@ -2,7 +2,22 @@
 (defconst *is-linux* (eq system-type 'gnu/linux))
 (defconst *is-windows* (or (eq system-type 'ms-dos) (eq system-type 'windows-nt)))
 
-;; encode
+(cond
+ (*is-windows*
+  ;; fix the  issue on Windows
+  ;; (setq buffer-file-coding-system 'utf-8)
+  (when (boundp 'w32-get-true-file-attributes)
+    (setq w32-get-true-file-attributes nil
+	      w32-pipe-read-delay 0
+	      w32-pipe-buffer-size (* 64 1024))))
+ (*is-mac*
+  ;; <macOS> Command -> Meta, Option -> Super
+  (setq mac-command-modifier 'meta
+	    mac-option-modifier 'super
+	    ns-use-native-fullscreen t))
+ (t nil))
+
+ ;; encode
 (prefer-coding-system 'utf-8)
 (unless *is-windows*
     (set-selection-coding-system 'utf-8))
--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -20,4 +20,39 @@
 
 (global-display-line-numbers-mode t)
 
+;; fount
+
+(defun font-installed-p (font-name)
+  "Check if font with FONT-NAME is available."
+  (find-font (font-spec :name font-name)))
+
+(defun kamijoucen/font-setup ()
+  "Font setup."
+
+  (interactive)
+  (when (display-graphic-p)
+    ;; Default font
+    (cl-loop for font in '("Courier Prime" "Cascadia Code" "Fira Code" "Jetbrains Mono" "Hack" "Source Code Pro" "Menlo" "Monaco" "Consolas")
+             when (font-installed-p font)
+             return (set-face-attribute 'default nil :family font))
+
+    ;; Unicode characters
+    (cl-loop for font in '("Segoe UI Symbol" "Symbola" "Symbol")
+             when (font-installed-p font)
+             return (set-fontset-font t 'unicode font nil 'prepend))
+
+    ;; Emoji
+    (cl-loop for font in '("Noto Color Emoji" "Apple Color Emoji")
+             when (font-installed-p font)
+             return (set-fontset-font t 'emoji (font-spec :family font) nil 'prepend))
+
+    ;; Chinese characters
+    (cl-loop for font in '("霞鹜文楷" "WenQuanYi Micro Hei" "PingFang SC" "Microsoft Yahei UI" "Microsoft Yahei" "STFangsong")
+             when (font-installed-p font)
+             return (progn
+                      (setq face-font-rescale-alist `((,font . 1.2)))
+                      (set-fontset-font t '(#x4e00 . #x9fff) (font-spec :family font))))))
+
+(add-hook 'emacs-startup-hook 'kamijoucen/font-setup)
+
 (provide 'init-ui)
